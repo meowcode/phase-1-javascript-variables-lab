9df90950847f1ddc3abfbd3d5dfc6f0092a5971f
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-10-29 15:06:46 -0400

    commit tree c9ac586d4057664ae905397cadf21e06e956720d

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-const companyName = 'Scuber';
+//const companyName = 'Scuber';
 
-let mostProfitableNeighborhood = 'Chelsea';
+//let mostProfitableNeighborhood = 'Chelsea';
 
-let companyCeo = 'Susan Smith';
+//let companyCeo = 'Susan Smith';
+
+
+const companyName = 'Scuber'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
